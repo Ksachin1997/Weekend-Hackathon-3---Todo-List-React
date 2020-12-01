1cf6f6e9f86955a8fa9a2a828c2c7914b52f35cd
Ksachin1997 <64680520+Ksachin1997@users.noreply.github.com>
2020-12-01 11:26:14 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
 import React from "react";
 import "./../styles/App.css";
 
-function App() 
-{
-	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
-	);
-}
+function App() {
+  const [listItem, setListItem] = React.useState("");
 
+  const [list, setList] = React.useState([]);
+
+  const handleChange = (event) => {
+    setListItem(event.target.value);
+  };
+
+  const handleClick = () => {
+    let newList = [...list];
+    newList.push(listItem);
+    setList(newList);
+  };
+
+  return (
+    <div id="main">
+      <textarea id="task" onChange={handleChange}></textarea>
+      <br />
+      <br />
+      <button id="btn" onClick={handleClick}>
+        ADD
+      </button>
+      <ul>
+        {list.map((item) => (
+          <li className="list">
+            {item}
+            <button className="edit">Edit</button>
+            <button className="delete">Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
