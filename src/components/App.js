ad2a73050a84392faf872338f25cecc6ca2e45a8
Ksachin1997 <64680520+Ksachin1997@users.noreply.github.com>
2020-12-01 12:15:28 +0530

    bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,33 @@ import React from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [listItem, setListItem] = React.useState("");
+  const [listItem, setListItem] = React.useState({ name: "", id: "" });
 
   const [list, setList] = React.useState([]);
 
   const handleChange = (event) => {
-    setListItem(event.target.value);
+    setListItem({ name: event.target.value, id: list.length });
   };
 
   const handleClick = () => {
+    if (listItem.name.trim().length === 0) {
+      return;
+    }
     let newList = [...list];
     newList.push(listItem);
+    setList(newList);
+  };
+
+  const handleDelete = (key) => {
+    let newList = [];
+
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].id === key) {
+        continue;
+      }
+      newList.push(list[i]);
+    }
+
     setList(newList);
   };
 
@@ -26,10 +42,12 @@ function App() {
       </button>
       <ul>
         {list.map((item) => (
-          <li className="list">
-            {item}
+          <li className="list" key={item.id}>
+            {item.name}
             <button className="edit">Edit</button>
-            <button className="delete">Delete</button>
+            <button className="delete" onClick={() => handleDelete(item.id)}>
+              Delete
+            </button>
           </li>
         ))}
       </ul>
